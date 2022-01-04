b93f56f05c20ba715bb6a60695364321d156f26a
Henrik Hautakoski <henrik@eossweden.org>
2022-01-04 09:54:34 +0100

    Convert hard tabs to soft.

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,69 +2,69 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	log "github.com/inconshreveable/log15"
-	"github.com/pborman/getopt/v2"
-	"github.com/onrik/ethrpc"
-	"github.com/eosswedenorg-go/haproxy"
-	"github.com/eosswedenorg-go/tcp_server"
+    "fmt"
+    "strings"
+    log "github.com/inconshreveable/log15"
+    "github.com/pborman/getopt/v2"
+    "github.com/onrik/ethrpc"
+    "github.com/eosswedenorg-go/haproxy"
+    "github.com/eosswedenorg-go/tcp_server"
 )
 
 var logger log.Logger
 
 func onConnect(c *tcp_server.Client) {
-	logger.Info("Client connected", "addr", c.Addr.String())
+    logger.Info("Client connected", "addr", c.Addr.String())
 }
 
 func onTcpMessage(c *tcp_server.Client, message string) {
 
-	status := haproxy.HealthCheckDown
-	client := ethrpc.New(strings.TrimSpace(message))
+    status := haproxy.HealthCheckDown
+    client := ethrpc.New(strings.TrimSpace(message))
 
-	resp, err := client.EthSyncing()
-	if err == nil {
-		if resp.IsSyncing == false {
-			status = haproxy.HealthCheckUp
-		}
-	} else {
-		logger.Warn(err.Error())
-	}
+    resp, err := client.EthSyncing()
+    if err == nil {
+        if resp.IsSyncing == false {
+            status = haproxy.HealthCheckUp
+        }
+    } else {
+        logger.Warn(err.Error())
+    }
 
-	logger.Info("Node status", "status", status)
+    logger.Info("Node status", "status", status)
 
-	// Report status to HAproxy
-	c.WriteString(fmt.Sprintln(status))
-	c.Close()
+    // Report status to HAproxy
+    c.WriteString(fmt.Sprintln(status))
+    c.Close()
 }
 
 func onDisconnect(c *tcp_server.Client, err error) {
-	if err == nil {
-		logger.Info("Client disconnected", "addr", c.Addr.String())
-	} else {
-		logger.Warn("Client disconnected", "addr", c.Addr.String(), "err", err)
-	}
+    if err == nil {
+        logger.Info("Client disconnected", "addr", c.Addr.String())
+    } else {
+        logger.Warn("Client disconnected", "addr", c.Addr.String(), "err", err)
+    }
 }
 
 func argv_listen_addr() string {
 
-	var addr string
+    var addr string
 
-	argv := getopt.Args()
-	if len(argv) > 0 {
-		addr = argv[0]
-	} else {
-		addr = "127.0.0.1"
-	}
+    argv := getopt.Args()
+    if len(argv) > 0 {
+        addr = argv[0]
+    } else {
+        addr = "127.0.0.1"
+    }
 
-	addr += ":"
-	if len(argv) > 1 {
-		addr += argv[1]
-	} else {
-		addr += "1301"
-	}
+    addr += ":"
+    if len(argv) > 1 {
+        addr += argv[1]
+    } else {
+        addr += "1301"
+    }
 
-	return addr
+    return addr
 }
 
 func main() {
@@ -76,7 +76,7 @@ func main() {
     getopt.FlagLong(&printUsage, "help", 'h', "Print this help text")
     getopt.FlagLong(&printVersion, "version", 'v', "Print version")
 
-	getopt.Parse()
+    getopt.Parse()
 
     if printUsage {
         getopt.Usage()
@@ -90,19 +90,19 @@ func main() {
 
     logger = log.New()
 
-	addr := argv_listen_addr()
-	server := tcp_server.New(argv_listen_addr())
+    addr := argv_listen_addr()
+    server := tcp_server.New(argv_listen_addr())
 
-	logger.Info(fmt.Sprintf("Listening on: %s", addr))
+    logger.Info(fmt.Sprintf("Listening on: %s", addr))
 
-	// TCP Client sends message.
-	server.OnConnect(onConnect)
-	server.OnMessage(onTcpMessage)
-	server.OnDisconnect(onDisconnect)
+    // TCP Client sends message.
+    server.OnConnect(onConnect)
+    server.OnMessage(onTcpMessage)
+    server.OnDisconnect(onDisconnect)
 
-	err := server.Listen()
+    err := server.Listen()
 
-	if err != nil {
-		logger.Error(err.Error())
-	}
+    if err != nil {
+        logger.Error(err.Error())
+    }
 }
